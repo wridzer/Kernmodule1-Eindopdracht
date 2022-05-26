ef207b1138374176217a2b0f81bb5573c35d548b
Andries <61500390+Emerald001@users.noreply.github.com>
2022-05-26 11:41:30 +0200

    Generate Navmesh

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -12,7 +12,7 @@ public interface Ipoolable {
 
 public class ObjectPool<T> where T : Ipoolable
 {
-    private List<T> activePool = new List<T>();
+    public List<T> activePool = new List<T>();
     private List<T> inactivePool = new List<T>();
 
     private T AddNewItemToPool() {
